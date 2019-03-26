86f81d14b8275913331bb61dfc0fcab2fae879c7
MacBook Pro <illshaadbudureea@gmail.com>
2019-03-26 09:45:29 +0100

    modification card

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import CryptoJS from "crypto-js";
+import Comic from './Components/comic';
+import {Container,Row,Button,
+ }from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const PRIV_KEY = "ce1693d8baa06f879abcab4c75c19e34006074db";
+const PUBLIC_KEY = "1415ee478404a80e6f620f034e526ca0";
+const ts = new Date().getTime();
+const hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+
+
+class App extends Component {
+  state = {
+    data: [],
+    total : 0
+  }
+  limit = 8;
+
+  componentDidMount() {
+    fetch(`http://gateway.marvel.com/v1/public/characters?ts=${ts}&offset=${this.state.offset}&limit=${this.limit}&apikey=${PUBLIC_KEY}&hash=${hash}`)
+    .then(response => response.json())
+    .then(data => this.setState({data: data.data.results , total : data.data.total /10}));
+    //Je divise par 10 pour limiter le nombre de page en dynamique //
+  }
+//Ma function qui me permet de faire la pagination à l'aide du limit et offset//
+  handleClick=(e)=>{
+    this.setState({
+    limit : 8 , offset : e.target.value
+    })
+    fetch(`http://gateway.marvel.com/v1/public/characters?ts=${ts}&offset=${e.target.value}&limit=${this.limit}&apikey=${PUBLIC_KEY}&hash=${hash}`)
+    .then(response => response.json())
+    .then(data => this.setState({data: data.data.results}));
+    }
+
+  render() {
+   //Boucle map qui me permet de afficher plusieurs characters ainsi de recupérer les info de l'api//
+    console.log(this.state.total)
+    const dataList = this.state.data.map((data,i)=>{
+     return (<Comic key={i} 
+      comicsTitle={data.name} 
+      comicsUrl={data.urls[1].url} 
+      comicsDesc={data.description} 
+      comicsImg={`${data.thumbnail.path}/portrait_xlarge.jpg`}/>)
+    })
+    // Ma boucle for qui me permet de diviser le nb total des characteres et ma limit que je souhaite affiché//
+    var buttonList = [];
+    var maxPage = this.state.total / this.limit;
+    for(var i=0; i<maxPage; i++) {
+      buttonList.push(<Button style={style.btnPage} value={i} onClick={this.handleClick}>{i+1}</Button>);
+    }
+
+    return (
+      <Container clasName='screen'>
+      <img className="marvel"src="./marvel_1.jpg" />
+      <Row> 
+        {dataList}
+      </Row>
+      {buttonList}
+      </Container>
+    )
+  }
+}
+
+var style= {
+  btnPage : {
+    backgroundColor: 'rgb(247, 143, 63)',
+    color: 'white',
+    marginLeft :'3px',
+    paddingRight :'25px',
+    width: '30px',
+    },
+  }
+
+export default App
+
+
+
+
---
+++ src/Components/comic.js
@@ -0,0 +1,78 @@
+import React, { Component,} from 'react';
+import {
+  Col,
+  Card,
+  CardImg,
+  CardBody,
+  Button,
+  CardTitle,
+  Modal ,
+  ModalHeader,
+  Badge,
+  ModalBody
+} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Comic extends Component {
+  constructor(props){
+    super(props)
+    //par defaut le modal et fermer //
+    this.state = {
+      modal: false,
+    }
+  }
+  //function qui me permet de ouvrir le modal//
+  ModalVisible =()=>{
+    console.log('Openmodal')
+    this.setState({modal :true})
+  }
+//function qui me permet de fermer le modal//
+  ModalInvisible =()=>{
+    console.log('Closemodal')
+    this.setState({modal :false})
+  }
+  render() {
+
+//Mise en place du Card et le Modal avec reactstrap //
+    return (
+      <Col xs="12" sm="6" md="4" lg="3">
+      <div style={{marginBottom:50}}>
+        <Card className='zoom' onClick={this.ModalVisible}>
+          <CardImg top width="100%" src={`${this.props.comicsImg}`} alt="Card image cap" style={{minHeight: 380}} />
+            <CardBody style={{height: 150}}>
+              <CardTitle>{this.props.comicsTitle}</CardTitle>
+            </CardBody>
+        </Card>
+
+        <Modal isOpen={this.state.modal}>
+        <ModalHeader style={{backgroundColor:"whitesmoke"}} ModalVisible={this.ModalVisible}>{this.props.comicsTitle}</ModalHeader>
+          <CardImg src={`${this.props.comicsImg}`} />
+          <h4><Badge style={style.button} href={`${this.props.comicsUrl}`} target="_blank" >Information</Badge></h4>
+          <ModalBody>{this.props.comicsDesc}</ModalBody>
+          <Button style={style} onClick={this.ModalInvisible}>x</Button>
+        </Modal>
+      </div>
+    </Col>
+    );
+  }
+}
+
+var style= {
+  backgroundColor: '#f78f3f',
+  color :'white',
+  position: 'absolute',
+  top: '2%',
+  left: '90%',
+  cursor: 'pointer',
+
+  button : {
+    backgroundColor: 'rgb(247, 143, 63)',
+    color: 'white',
+    marginLeft: '25%',
+    marginTop: '5%',
+    width: '50%',
+    },
+  }
+
+
+export default Comic;
